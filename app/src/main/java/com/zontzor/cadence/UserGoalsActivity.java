8d157c5a16508429d40f09a1325d647ef342a575
Zontzor <alex.kiernan@mydit.ie>
2015-11-14 17:36:39 +0000

    Added project files so far

---
+++ app/src/main/java/com/zontzor/cadence/UserGoalsActivity.java
@@ -0,0 +1,11 @@
+package com.zontzor.cadence;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UserGoalsActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_goals);
+    }
+}
