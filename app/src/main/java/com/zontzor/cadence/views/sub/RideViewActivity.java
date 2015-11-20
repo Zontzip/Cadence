4556c93f37df6125dad3ed11d2d0b12a16cd45e1
Alex Kiernan <zontzor@gmail.com>
2015-11-20 14:21:20 +0000

    Updates

---
+++ app/src/main/java/com/zontzor/cadence/views/sub/RideViewActivity.java
@@ -0,0 +1,23 @@
+package com.zontzor.cadence.views.sub;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.zontzor.cadence.R;
+
+public class RideViewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ride_view);
+
+
+    }
+
+}
